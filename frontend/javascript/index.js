8b1bd34e59288d8f93114666ffc08b9cb55ffbf7
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2021-01-07 19:52:29 +0100

    début création homepage

---
+++ frontend/javascript/index.js
@@ -0,0 +1,9 @@
+/*function accessNetwork(){
+    let connectionButton = document.getElementById('go-to-network')
+    console.log(connectionButton)
+    connectionButton.addEventListener('submit', function(){
+        window.location = 'reseau.html'
+    })
+}
+
+accessNetwork()*/
