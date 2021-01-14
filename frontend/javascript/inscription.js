905aab55199bdfc55469831863744255080fa019
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2021-01-14 16:24:42 +0100

    signup via front end

--- frontend/javascript/inscription.js
+++ frontend/javascript/inscription.js
@@ -1,10 +1,16 @@
 goNetwork()
 
-function getUserLogin(){
+function getUserSignup(){
     let mail = document.getElementById('email').value
+    let lastName = document.getElementById('lastName').value
+    let firstName = document.getElementById('firstName').value
+    let pseudo = document.getElementById('pseudo').value
     let password = document.getElementById('password').value
     let user = {
         email : mail,
+        lastName : lastName,
+        firstName : firstName,
+        pseudo : pseudo,
         password : password
     }
     console.log(user)
@@ -12,9 +18,9 @@ function getUserLogin(){
 }
 
 function login(form){
-    let data = send(`http://localhost:3000/api/login`,form)
+    let data = send(`http://localhost:3000/api/signup`,form)
     data.then (user =>{
-        let pseudo = user.userPseudo;
+        let pseudo = user.pseudo;
         window.location = `reseau.html?pseudo=${pseudo}`
         console.log(pseudo)
         console.log(user)
@@ -26,7 +32,6 @@ function goNetwork(){
     let button = document.getElementById("go-to-network")
     button.addEventListener('click', function (e) {
         e.preventDefault()
-        getUserLogin()
+        getUserSignup()
     })
 }
-
