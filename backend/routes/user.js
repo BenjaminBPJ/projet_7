7ccbb4dd0ce42f285df98ab6e489530b44d52891
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2021-04-01 18:32:36 +0200

    ajout limiter connection

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const limiter = require('../middleware/limiter');
 const multer = require('../middleware/multer-config');
 
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', limiter, userCtrl.login);
 router.delete('/:id', auth, userCtrl.deleteUser);
 router.put('/:id', auth, userCtrl.updateDescription);
 router.post('/:id', auth, userCtrl.updatePhoto);
