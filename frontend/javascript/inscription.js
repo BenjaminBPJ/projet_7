4e2822e98ac36e4d0c09bb6084a6de48bd608971
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2021-04-05 14:13:19 +0200

    validation des saisies inscription

--- frontend/javascript/inscription.js
+++ frontend/javascript/inscription.js
@@ -1,40 +1,127 @@
-goNetwork()
+function signup(form) {
+    let data = send(`http://localhost:3000/api/signup`,form);
+    data.then (user => {
+        let userId = user.userId;
+        window.location = `reseau.html/userId=${userId}`;
+     
+        userId = localStorage.setItem('userId', userId);
+        userId = JSON.stringify(userdId);
+        let token = user.token;
+        console.log(token);
+        token = localStorage.setItem('token', token);
+        token = JSON.stringify(token);
+    })
+    .catch((error) => ({ error }));
+}
 
-function getUserSignup(){
-    let mail = document.getElementById('email').value
-    let lastName = document.getElementById('lastName').value
-    let firstName = document.getElementById('firstName').value
-    let password = document.getElementById('password').value
+function getUserSignup() {
+    let mail = document.getElementById('email').value;
+    let lastName = document.getElementById('lastName').value;
+    let firstName = document.getElementById('firstName').value;
+    let password = document.getElementById('password').value;
     let user = {
         email : mail,
         lastName : lastName,
         firstName : firstName,
         password : password
-    }
-    console.log(user)
-    signup(user)
+    };
+    console.log(user);
+    signup(user);
 }
 
-function signup(form){
-    let data = send(`http://localhost:3000/api/signup`,form)
-    data.then (user => {
-        let userId = user.userId
-        window.location = `reseau.html/userId=${userId}`
-     
-        userId = localStorage.setItem('userId', userId)
-        userId = JSON.stringify(userdId)
-        let token = user.token
-        console.log(token)
-        token = localStorage.setItem('token', token)
-        token = JSON.stringify(token) 
-    })
-    .catch((error) => ({ error }));
-}
-
-function goNetwork(){
-    let button = document.getElementById("go-to-network")
+function goNetwork() {
+    let button = document.getElementById("go-to-network");
     button.addEventListener('click', function (e) {
-        e.preventDefault()
-        getUserSignup()
+        e.preventDefault();
+        getUserSignup();
     })
 }
+
+function emailValide(inputEmail) {
+    let emailRegExp = /^[\w-]+(\.[\w-]+)*@groupomania.fr?$/;
+    let testEmail = emailRegExp.test(inputEmail.value);
+    let small = document.getElementById(`small-email`);
+    if (testEmail) {
+        small.innerHTML = `Email valide.`;
+        return true;
+    } else {
+        small.innerHTML = `Merci d'écrire une adresse mail de notre entreprise valide.`;
+        return false;
+    };
+};
+
+function lastNameValide(inputName) {
+    let nameRegExp = new RegExp(`^[A-Za-zÀ-ÖØ-öø-ÿ-]+( {0,1}[A-Za-zÀ-ÖØ-öø-ÿ-]?){1,60}$`, 'g');
+    let testName = nameRegExp.test(inputName.value);
+    let small = document.getElementById(`small-lastName`);
+    if (testName) {
+        small.innerHTML = `Nom valide.`;
+        return true;
+    } else {
+        small.innerHTML = `Merci d'écrire votre nom en toutes lettres.`;
+        return false;
+    };
+};
+
+function firstNameValide(inputName) {
+    let nameRegExp = new RegExp(`^[A-Za-zÀ-ÖØ-öø-ÿ-]+( {0,1}[A-Za-zÀ-ÖØ-öø-ÿ-]?){1,60}$`, 'g');
+    let testName = nameRegExp.test(inputName.value);
+    let small = document.getElementById(`small-firstName`);
+    if (testName) {
+        small.innerHTML = `Prénom valide.`;
+        return true;
+    } else {
+        small.innerHTML = `Merci d'écrire votre prénom en toutes lettres.`;
+        return false;
+    };
+};
+
+function passwordValide(inputPassword) {
+    let passwordRegExp = new RegExp(`^(?=.{8,})(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$`,`g`);
+    let testPassword = passwordRegExp.test(inputPassword.value);
+    let small = document.getElementById(`small-password`);
+    if (testPassword) {
+        small.innerHTML = `Mot de passe valide.`;
+        return true;
+    } else {
+        small.innerHTML = `Votre mot de passe doit contenir au moins un chiffre, une minuscule, une majuscule et être composé de 8 caractères minimum !`;
+        return false;
+    };
+};
+
+function confirmPassword () {
+    let mdp = document.getElementById('password').value;
+    let confirmMdp = document.getElementById('confirm-password').value;
+    let small = document.getElementById(`small-confirm-password`);
+
+    if ( mdp === confirmMdp ) {
+        small.innerHTML = `Votre mot de passe n'est pas le même.`;
+        return false;
+    }else {
+        small.innerHTML = `Votre mot de passe est correct.`;
+        return true;
+    };
+};
+
+function validForm() {
+    let form = document.getElementById("sign-up-form");
+    // validation du formulaire
+    form.email.addEventListener('change', function () {
+        emailValide(this);
+    });
+    form.lastName.addEventListener('change', function () {
+        lastNameValide(this);
+    });
+    form.firstName.addEventListener('change', function () {
+        firstNameValide(this);
+    });
+    form.password.addEventListener('change', function () {
+        passwordValide(this);
+    });
+    form.confirm-password.addEventListener('change', function () {
+        confirmPassword(this);
+    });
+};
+
+goNetwork();
+validForm();
--- frontend/javascript/multi-fonction.js
+++ frontend/javascript/multi-fonction.js
@@ -4,3 +4,4 @@ function backNetwork() {
         window.location = `reseau.html`;
     });
 };
+
