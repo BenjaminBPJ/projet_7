8d95d8de39ea9845031cb6e678e51f89cc88f146
BenjaminBPJ <pierrejeanbenjamin@hotmail.fr>
2021-06-24 11:13:46 +0200

    travail refont page accueil

--- frontend/javascript/view.js
+++ frontend/javascript/view.js
@@ -6,23 +6,22 @@ getPost = (value) => {
 
     article.innerHTML = `<header class ="header-publication">
                             <h3 class="auteur-publication">${value.firstName} ${value.lastName}</h3>
-                            <h4 id="date-publication${value.id}">Posté il y a ${calcTime(value.datePublication)}</h4>                                                     
+                            <h4 id="date-publication${value.id}" class="date-publication-post">Posté il y a ${calcTime(value.datePublication)}</h4>                                                     
                         </header>
                         <div class="texte-image-publication">
-                            <h5 class="titre-publication${value.id}">${value.titre}</h5>
-                            <p class="content-publication${value.id}">${value.publication}</p>`;
+                            <h5 class="titre-publication${value.id} titre-publication">${value.titre}</h5>
+                            <p class="content-publication${value.id} content-publication">${value.publication}</p>`;
 
     if (value.imageUrl) {
-        article.innerHTML += `<img src="${value.imageUrl}" class="image-publication${value.id}" alt="image illustrant l'article" />`;
+        article.innerHTML += `<img src="${value.imageUrl}" class="image-publication${value.id} image-publication" alt="image illustrant l'article" />`;
     };
 
     article.innerHTML += `</div>
                           <div class="input-new-comment">
                              <input type="text" placeholder="ecrivez votre commentaire" class="input-commentaire" id="send-comment${value.id}" />
                              <input type="submit" id="sending-comment${value.id}" value="commenter" />
-                             <small id="small-send-comment${value.id}"></small>
+                             <small id="small-send-comment${value.id}" class="small"></small>
                          </div>`;
-
 };
 
 deleteUpdatePostIcon = (value) => {
@@ -33,7 +32,7 @@ deleteUpdatePostIcon = (value) => {
                         <i class="fas fa-trash-alt" id="delete-publication${value.id}"></i>
                         <small id="small-delete-publication"></small>
                         <i class="fas fa-edit" id="update-publication${value.id}"></i>
-                        <small id="small-update-publication"></small>
+                        <small id="small-update-publication" class="small"></small>
                      </div>`;
 };
 
@@ -43,7 +42,7 @@ formUpdatePost = (value) => {
     form.setAttribute('id', `form-modale-update${value.id}`);
     form.classList.add('form-modale');
 
-    form.innerHTML = `<form>
+    form.innerHTML =    `<form aria-hidden="true">
                             <label for="old-title">Ancien titre</label><br>
                             <p id="old-tile">${value.titre}</p>
                             <label for="new-title">Nouveau titre</label><br>
@@ -51,17 +50,13 @@ formUpdatePost = (value) => {
                             <label for="old-post">ancien article</label><br>
                             <p id="old-post">${value.publication}</p>
                             <label for="new-post">Nouvel article</label><br>
-                            <textarea id="new-post" class="input-update-form" placeholder="écrivez votre nouvel article"></textarea><br>
-                            
-                            
+                            <textarea id="new-post" class="input-update-form" placeholder="écrivez votre nouvel article"></textarea><br>                            
                             <label for="new-image">Nouvelle image</label><br>
                             <input type="file" class="input-update-form" id="new-image"/><br>
                             <button  id="close-form-update-post">Fermer</button>
                             <button  id="send-form-update-post${value.id}">Modifier</button>
-                            <small id="small-form-update"></small>
+                            <small id="small-form-update" class="small"></small>
                         </form>`;
-
-    // ` if (value.imageUrl) {form.innerHTML += `<label for="old-image">ancienne image partagée</label><br<img src="${value.imageUrl}" id="old-image" />`}                  form.innerHTML += `
 };
 
 getComment = (value) => {
@@ -71,11 +66,11 @@ getComment = (value) => {
     comment.setAttribute(`id`, `commentaire${value.id}`);
     comment.classList.add("commentaire");
 
-    comment.innerHTML = `<header class ="header-commentaire">
+    comment.innerHTML =     `<header class ="header-commentaire">
                                 <h3 class="auteur-commentaire">${value.firstName} ${value.lastName}</h3>
                                 <h4 class="date-commentaire">Il y a ${calcTime(value.publiAt)}</h4>
                             </header>
-                            <p>${value.content}</p>`;
+                            <p class="comment-content">${value.content}</p>`;
 
     let currentUser = localStorage.getItem('userId');
     let roleUser = localStorage.getItem('role');
@@ -84,7 +79,7 @@ getComment = (value) => {
                                   <small id="small-delete-comment"></small><br>
                                   <input  id="comment-to-update${value.id}" />
                                   <input type="submit" id="update-comment-done${value.id}" value="Modifier commentaire"/>
-                                  <small id="small-update-comment${value.id}"></small>`;
+                                  <small id="small-update-comment${value.id}" class="small"></small>`;
     };
 };
 
@@ -144,21 +139,21 @@ userEditProfil = (value) => {
     article.classList.add("profil-update");
 
     article.innerHTML = `  <div class="image-for-update">
-                            <p class="user-image-edit-profil">Choississez un fichier si vous voulez modifier votre image</p>
-                            <input type="file" name="image" id="image-user" accept="image/*" />   
+                                <p class="user-image-edit-profil">Choississez un fichier si vous voulez modifier votre image</p>
+                                <input type="file" name="image" id="image-user" accept="image/*" />   
                             </div>                            
                             <h2 class="profil-title">${value.result[0].firstName} ${value.result[0].lastName}</h2>
                             <form>
-                            <label for="old-description">Description : </label>
-                            <p id="old-description" >${value.result[0].description}</p>
-                            <label for="new-description"></label>
-                            <textarea id="new-description" type="text" rows="5" placeholder="Veuillez renseigner votre nouvelle description ..."></textarea><br>
-                            <button type="submit" id="send-profil">Valider</button>
+                                <label for="old-description">Description : </label>
+                                <p id="old-description" >${value.result[0].description}</p>
+                                <label for="new-description"></label>
+                                <textarea id="new-description" type="text" rows="5" placeholder="Veuillez renseigner votre nouvelle description ..."></textarea><br>
+                                <button type="submit" id="send-profil">Valider</button>
                             </form>
-                            <small id="small-update-user"></small>   
+                            <small id="small-update-user" class="small"></small>   
                             <div class="div-button-update-user">
-                            <button id="delete-account" class="button-update-user">Suppression du compte</button><br>     
-                            <button id="back-to-profil" class="button-update-user">Retour au profil</buttton>  
+                                <button id="delete-account" class="button-update-user">Suppression du compte</button><br>     
+                                <button id="back-to-profil" class="button-update-user">Retour au profil</buttton>  
                             </div>                 
                         `;
                         noImageUpdateUser(value)
@@ -226,12 +221,3 @@ calcTime = (time) => {
             return undefined;
     };
 };
-
-/*<div class="profil-card">
-                            <img src= "${value.result[0].imageUrl}" class="photo-profil"/>
-                            <h2 class="profil-title">${value.result[0].firstName} ${value.result[0].lastName}</h2>
-                            <p class="profil-description"> Description : </p>
-                            <p id="text-description" class="profil-description" rows="5">${value.result[0].description}</p><br>
-                            <button type="submit" id="goToEdit">Modification de votre profil</buttton>
-                            <button type="submit" id="back-to-network">retour</buttton>
-                            </div>*/
